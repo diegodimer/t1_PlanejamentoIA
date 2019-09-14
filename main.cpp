9c7c797a54638c4396a03d0d4c92769e5fe19679
Diego Dimer <diego.dimer@ufrgs.br>
2019-09-14 18:01:17 -0300

    Nova funcao de succ retornando vetor de estados

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include <bits/stdc++.h>
 #include <cstdio>
 
+#define PUZZLE 3
+#define GOAL 305419896
 using namespace std;
 /* 
 
@@ -21,14 +23,12 @@ int calc_h(State);
 bool is_goal(State);
 void succ(Node, vector<Node>);
 
-// mapa com todos os estados (funciona parecido com um hash set)
-unordered_map<unsigned long long, State *> estados;
-
 class State
 {
 
   unsigned long long state; // estado representado com 64-bit
   int h;
+  unsigned char posicao_zero; // os 4 primeiros bits são a linha, os 4 seguintes a coluna
 
 public:
   State(string _state)
@@ -41,15 +41,41 @@ public:
         unsigned char num = (unsigned char)_state[i] - '0';
         state += num;
         state = state << 4;
+
+        if (_state[i] == '0')
+        {
+          unsigned char should_linha; // linha que devia estar
+          int n = i / 6;
+          if (n < 1)
+          {
+            should_linha = 0x0;
+          }
+          else if (n < 2)
+          {
+            should_linha = 0x1;
+          }
+          else
+            should_linha = 0x2;
+
+          unsigned char should_coluna = (i / 2) % 3;
+          posicao_zero = (should_linha << 4) | should_coluna;
+        }
       }
     }
 
     state = state >> 4; // um if ali testado n vezes é mais facil fazer 4 shifts pro outro lado depois
     h = calc_h(*this);
   }
-
+  State(unsigned long int _state, unsigned char _posicao_zero)
+  {
+    state = _state;
+    posicao_zero = _posicao_zero;
+    h = calc_h(*this);
+  }
+  State() {}
   long long getState() const { return state; }
   int getH() const { return h; }
+  unsigned char getPosZero() const { return posicao_zero; }
 
   /* Função que printa o estado (fins de debug) */
   void printState() const
@@ -61,27 +87,33 @@ public:
          << "\n";
     cout << "Representação do estado: "
          << "\n";
-    for (int i = 0; i < 3; i++)
+    for (int i = 0; i < PUZZLE; i++)
     {
 
-      for (int j = 0; j < 3; j++)
+      for (int j = 0; j < PUZZLE; j++)
       {
         number = state & position;     // a mascara pega o primeiro numero
         number = number >> 4 * indice; // e shifta ele até a posição certa
         indice -= 1;
         int should_linha;
-        if (number / 3 < 1)
+        if (number / PUZZLE < 1)
         {
           should_linha = 0;
         }
-        else if (number / 3 < 2)
+        else if (number / PUZZLE < 2)
         {
           should_linha = 1;
         }
-        else
+        else if (number / PUZZLE < 3)
+        {
           should_linha = 2;
+        }
+        else
+        {
+          should_linha = 3;
+        }
 
-        int should_coluna = number % 3;
+        int should_coluna = number % PUZZLE;
         int h = abs(should_linha - i) + abs(should_coluna - j);
         cout << number << " (h(n): " << h << ")   ";
         position = position >> 4; // move a mascara p/ pegar o próximo numero
@@ -91,20 +123,25 @@ public:
     cout << "========================"
          << "\n";
   }
-
 };
 
 class Node
 {
   int g;
   int f;
-  State state;
+  State *state;
 
 public:
   int getG() const { return g; }
-  int getF() const { return g + state.getH(); }
-  int getH() const { return state.getH(); }
-  State getState() const { return state; }
+  int getF() const { return g + state->getH(); }
+  int getH() const { return state->getH(); }
+  void setG(int _g) { g = _g; }
+  void setF(int _f) { f = _f; }
+  State getState() const { return *state; }
+  Node(State *_s)
+  {
+    state = _s;
+  }
 };
 
 class Comparador
@@ -144,7 +181,7 @@ public:
 
 bool is_goal(State s)
 {
-  return s.getState() == 305419896;
+  return s.getState() == GOAL;
 }
 
 int calc_h(State s)
@@ -154,15 +191,15 @@ int calc_h(State s)
   unsigned long long state = s.getState();
   int h_ac = 0;
   int indice = 8;
-  for (int i = 0; i < 3; i++)
+  for (int i = 0; i < PUZZLE; i++)
   {
-    for (int j = 0; j < 3; j++)
+    for (int j = 0; j < PUZZLE; j++)
     {
       number = state & position;     // a mascara pega o primeiro numero
       number = number >> 4 * indice; // e shifta ele até a posição certa
       indice -= 1;
       int should_linha; // linha que devia estar
-      int n = number / 3;
+      int n = number / PUZZLE;
       if (n < 1)
       {
         should_linha = 0;
@@ -171,10 +208,15 @@ int calc_h(State s)
       {
         should_linha = 1;
       }
-      else
+      else if (n < 3)
+      {
         should_linha = 2;
-
-      int should_coluna = number % 3;
+      }
+      else
+      {
+        should_linha = 3;
+      }
+      int should_coluna = number % PUZZLE;
       int h = abs(should_linha - i) + abs(should_coluna - j);
       h_ac += h;
       position = position >> 4; // move a mascara p/ pegar o próximo numero
@@ -183,11 +225,73 @@ int calc_h(State s)
   return h_ac;
 }
 
+
+State swap_board(unsigned long long state_original, short int zero_linha, short int zero_coluna, short int new_zero_linha, short int new_zero_coluna, short int pos_zero_state)
+{
+  unsigned long long mask = 0xF;
+  short int pos_array_original = 3 * new_zero_linha + new_zero_coluna;                   // posição no array do state onde está o numero q vai trocar com o 0
+  unsigned long long mask_dois = mask << 4 * (PUZZLE * PUZZLE - 1 - pos_array_original); // mask_dois é do tipo 0000F000 e F é onde está o número que vai trocar com o 0
+  mask_dois = mask_dois & state_original;                                                // mask_dois tem o número que  vai trocar com o zero
+  unsigned long long new_state = state_original & (~mask_dois);                          // faz o AND com a negação da mascara para zerar onde está o número que ia trocar com o zero
+  // shifto o mask_dois até onde estava o zero antes e faço or com new_state
+  short int offset = pos_array_original - pos_zero_state; // nao existe shift negativo
+  if (offset < 0)
+  {
+    mask_dois = mask_dois >> abs(offset) * 4;
+  }
+  else
+  {
+    mask_dois = mask_dois << offset * 4;
+  }
+  new_state = mask_dois | new_state; // coloca o numero no lugar do zero antigo
+
+  unsigned char new_zero_pos = (new_zero_linha << 4) + new_zero_coluna;
+  State novo_estado = State(new_state, new_zero_pos);
+
+  // novo_estado.printState();
+  return novo_estado;
+}
+
+// gera os sucessores de um nodo n
+void succ(Node n, vector<State> *suc)
+{
+  State state = n.getState();
+  unsigned long long state_original = state.getState();
+  unsigned char zero_posicao = state.getPosZero();
+  short int zero_linha = (short int)(zero_posicao >> 4);   // empurra 4 bits pra pegar os 4 mais significativos (linha)
+  short int zero_coluna = (short int)(0xF & zero_posicao); // mascara 1111 pra pegar os ultimos 4 bits (coluna)
+  short int pos_zero_state = 3 * zero_linha + zero_coluna;
+
+  if (zero_linha != PUZZLE)
+  {
+    // não ta na última linha, troca com o de baixo
+    suc->push_back(swap_board(state_original, zero_linha, zero_coluna, zero_linha + 1, zero_coluna, pos_zero_state));
+  }
+
+  if (zero_linha != 0)
+  { // não ta na primeira linha, troca com o de cima
+    suc->push_back(swap_board(state_original, zero_linha, zero_coluna, zero_linha - 1, zero_coluna, pos_zero_state));
+  }
+
+  if (zero_coluna != 0)
+  { // não ta na primeira linha, troca com o de cima
+    suc->push_back(swap_board(state_original, zero_linha, zero_coluna, zero_linha, zero_coluna - 1, pos_zero_state));
+  }
+
+  if (zero_coluna != PUZZLE)
+  { // não ta na primeira linha, troca com o de cima
+    suc->push_back(swap_board(state_original, zero_linha, zero_coluna, zero_linha, zero_coluna + 1, pos_zero_state));
+  }
+};
+
 int main()
 {
   State teste = State("7 2 4 5 0 6 8 3 1"); // esse estado é 30674020401 em decimal
   State teste_dois = State("0 1 2 3 4 5 6 7 8");
-  cout << " " << calc_h(teste) << " teste_dois: " << calc_h(teste_dois) << endl;
+  // State teste_tres = State("1 0 2 3 4 5 6 7 8");
+  // State teste_quatro = State("1 2 0 3 4 5 6 7 8");
+  // State teste_cinco = State("1 2 3 0 4 5 6 7 8");
+   //cout << " " << calc_h(teste) << " teste_dois: " << calc_h(teste_dois) << endl;
 
   // cout << teste.is_goal() << " " << teste_dois.is_goal() << "\n";
   // teste_dois.printState();
@@ -196,7 +300,35 @@ int main()
   // if (mapa.end() == mapa.find(e)){
   //   cout <<"a";
   // }
+  // priority_queue<Node, vector<Node>, Comparador> open;
+  // unordered_map<unsigned long long, State> closed;
 
-  priority_queue<Node, vector<Node>, Comparador> open;
-  unordered_map<unsigned long long, State> closed;
+  cout << "\nEstado inicial:: " << endl;
+  teste.printState();
+
+  vector<State> sucessores;
+  Node nodo = Node(&teste);
+  succ(nodo, &sucessores);
+
+  cout << "Sucessores::: " << endl;
+  while (!sucessores.empty())
+  {
+    State estado;
+    estado = sucessores.back();
+    estado.printState();
+    sucessores.pop_back();
+    
+  }
+
+  // Node nodo2 = Node(&teste_dois);
+  // succ(nodo2, sucessores);
+
+  // Node nodo3 = Node(&teste_tres);
+  // succ(nodo3, sucessores);
+
+  // Node nodo4 = Node(&teste_quatro);
+  // succ(nodo4, sucessores);
+
+  // Node nodo5 = Node(&teste_cinco);
+  // succ(nodo5, sucessores);
 }
